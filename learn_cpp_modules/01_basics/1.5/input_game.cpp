0734896bf1a8bd72d181b637adf53af1cb3c496e
JesseDance <dance.jbd@gmail.com>
2026-03-05 22:43:03 -0600

    more iostream

--- learn_cpp_modules/01_basics/1.5/input_game.cpp
+++ learn_cpp_modules/01_basics/1.5/input_game.cpp
@@ -6,5 +6,13 @@ int main() {
 	std::cin >> x; 
 	std::cout << "Value stored: " << x << '\n';
 
+
+	std::cout << "Now enter three numbers: ";
+	int i{};
+	int j{};
+	int k{};
+	std::cin >> i >> j >> k;
+	std::cout << "You entered " << i << ", " << j << ", and "<< k << ".\n";
+
 	return 0;
 }

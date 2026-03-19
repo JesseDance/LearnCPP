ee4c3f85851da64e1474e764b9e8e04d8270e152
JesseDance <dance.jbd@gmail.com>
2026-03-18 23:29:56 -0500

    added 2.4 example

---
+++ learn_cpp_modules/02_functions_and_files/2.4/doubleNumber.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int doubleNumber(int x) {
+	return 2 * x;
+}
+
+
+int main() {
+	std::cout << "Enter a number: ";
+
+	int input{};
+	std::cin >> input;
+
+	std::cout << "You entered the number: " << input << '\n';
+	std::cout << input << " doubled is " << doubleNumber(input) << '\n';
+	
+	return 0;
+}

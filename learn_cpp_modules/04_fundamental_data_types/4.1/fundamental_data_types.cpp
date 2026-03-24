e5b8a28b3cb7d4a2a59c2ce728540040fa948b8f
JesseDance <dance.jbd@gmail.com>
2026-03-23 23:32:11 -0500

    added 4.1

---
+++ learn_cpp_modules/04_fundamental_data_types/4.1/fundamental_data_types.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+// #include <cstddef>
+#include <cstddef>
+#include <uchar.h>
+
+int main() {
+  float a = 3.14159;
+  double b = 3.14159;
+  long double c = 3.14159;
+
+  bool d = true;
+
+  char e = 'e';
+  wchar_t f = 'c';
+  char8_t g = 'y';
+  char16_t h = 'x';
+  char32_t i = 'z';
+
+  short int j = 64;
+  int k = 56;
+  long int l = 2346;
+  long long int m = 234523462;
+
+  std::nullptr_t null_pointer;
+
+  return 0;
+}
